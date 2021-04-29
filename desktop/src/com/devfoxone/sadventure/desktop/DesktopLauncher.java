287b1f456a8a17dff7418cd2823ef2b255bbf4ee
FWI-95 <devfoxone@gmail.com>
2021-04-29 22:25:17 +0200

    Rename

--- desktop/src/com/devfoxone/sadventure/desktop/DesktopLauncher.java
+++ desktop/src/com/devfoxone/sadventure/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package com.devfoxone.sadventure.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.devfoxone.sadventure.ShinyAdventure;
+import com.devfoxone.AdvancedGameTut.Drop;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
@@ -10,6 +10,6 @@ public class DesktopLauncher {
 		config.title = "Shiny Adventure";
 		config.width = 800;
 		config.height = 480;
-		new LwjglApplication(new ShinyAdventure(), config);
+		new LwjglApplication(new Drop(), config);
 	}
 }
